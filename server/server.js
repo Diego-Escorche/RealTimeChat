612467088189e9336e899d56d1b790511eea876c
Diego-Escorche <diegoescorche11@gmail.com>
2025-02-25 18:20:52 -0300

    Add Socket.IO for real-time chat functionality and create client interface

--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
 import express from "express";
 import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 1000,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user has connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message: " + msg);
+  });
+});
+
 app.use(logger("dev"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Chat</h1>");
+  res.sendFile(process.cwd() + "/client/index.html");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port: http://localhost:${port}`);
 });
